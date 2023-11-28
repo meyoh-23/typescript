b26b76064020f749288f1e472e8c32630ea8ea36
Felix <moxiemeyoh@gmail.com>
2023-11-28 12:05:13 +0300

    add => enums basics in ts

---
+++ enums-typescripts/enums.ts
@@ -0,0 +1,22 @@
+// enums exist to restrict a person's choices
+// documentation on enums can be helpful on more learning
+// the line below generates an ify code
+enum Seatchoice {
+    AISLE = 3,
+    MIDDLE = "middle",
+    WINDOW = 10,
+    FORTH = 23
+}
+
+const hcSeat = Seatchoice.AISLE;
+
+
+// a simpler code is generated
+/* const enum Seatchoice {
+    AISLE = 3,
+    MIDDLE = "middle",
+    WINDOW = 10,
+    FORTH = 23
+}
+
+const hcSeat = Seatchoice.AISLE; */
